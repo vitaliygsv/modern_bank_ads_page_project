8c4c9dc8d962a120c5a4887531a710df146a16e8
Vitaliy Gosteev <vitaliygosteev@gmail.com>
2021-03-23 23:55:00 +0500

    added smooth scroll feature

--- script.js
+++ script.js
@@ -50,3 +50,11 @@ document
   .addEventListener("click", function () {
     message.remove();
   });
+
+// Smooth Scrolling
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function () {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
