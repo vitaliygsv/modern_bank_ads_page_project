64a60f6520416da93a8f2898d496d1c5fcb61b57
Vitaliy Gosteev <vitaliygosteev@gmail.com>
2021-04-05 19:58:58 +0500

    added Tabbed component

--- script.js
+++ script.js
@@ -68,3 +68,23 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+// Tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainter = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainter.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  if (!clicked) return;
+
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  clicked.classList.add("operations__tab--active");
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
