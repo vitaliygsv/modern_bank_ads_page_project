304a8483fb7910b9c446b31a077ad8882d5c8048
Vitaliy Gosteev <vitaliygosteev@gmail.com>
2021-04-07 20:24:30 +0500

    added revealing sections on scroll feature

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const nav = document.querySelector(".nav");
 const navHeight = nav.getBoundingClientRect().height;
 
 const btnScrollTo = document.querySelector(".btn--scroll-to");
+const allSections = document.querySelectorAll(".section");
 const section1 = document.querySelector("#section--1");
 
 const tabs = document.querySelectorAll(".operations__tab");
@@ -121,3 +122,23 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+// Revealing sections on scroll
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
